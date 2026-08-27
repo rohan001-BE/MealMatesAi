e0ec6eb9f77be7b32a56909828c1926808c1da01
Rohan Bin Ejaz <honeybhai0990@gmail.com>
2026-08-27 23:54:04 +0500

    fix: update frontend Firebase credentials to match main meal-mates-16030 project

--- frontend/src/config/firebaseConfig.js
+++ frontend/src/config/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCpqZM6i33s692uhB6VBdnbbOW_ImROIhk",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAhqdttQjk8YEaYzXboBmVOF83ATK29Te4",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "meal-mates-16030.firebaseapp.com",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "meal-mates-16030",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "meal-mates-16030.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1064353787393",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1064353787393:web:f27f9906b79d3d3bc27ec1",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-11HTNN68NP",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "meal-mates-16030.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "466012209013",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:466012209013:web:6ae368531d05cef29e9d3c",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-Y8N33GLFCS",
 };
 
 let app = null;
